dcc1ab7dcb4103642dd540c68ecb3d15c66d86ce
RailUwU <134000320+RailUwU@users.noreply.github.com.>
2025-04-05 13:22:05 +0530

    Frontend setup and creation of products

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/products",productRoutes);
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     connectDB();
     console.log("Server Started at http://localhost:5000")
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@chakra-ui/react";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./Components/Navbar";
+import {Route,Routes} from 'react-router-dom';
+
+function App() {
+
+  return (
+      <Box minH={"100vh"}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/create" element={<CreatePage/>}/>
+        </Routes>
+      </Box>
+  );
+}
+
+export default App
